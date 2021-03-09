5fead0050492a7227ada07e9ca11e30ce666ad54
Josh Barnes <joshbarnes@gmail.com>
2021-03-09 11:30:57 +0000

    Add service constants

---
+++ Avicenna.TechTest/Constants.cs
@@ -0,0 +1,10 @@
+namespace Avicenna.TechTest
+{
+    public static class Constants
+    {
+        public static readonly string[] AvailableServices = { 
+					"Stop smoking service",
+					"Weight-loss service",
+					"Travel vaccinations" }; 
+    }
+}
